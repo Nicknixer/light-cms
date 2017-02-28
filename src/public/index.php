71ebfb7b9ba3dd885e69797af0d124c382e00db8
Nick Sergeychuk <nicknixer@gmail.com>
2017-02-28 16:33:48 +0300

    add entry point and routes

---
+++ src/public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function (Request $request, Response $response) {
+
+    $response->getBody()->write('Index');
+
+    return $response;
+});
+
+$app->get('/{page}', function (Request $request, Response $response) {
+    $page = $request->getAttribute('page');
+    $response->getBody()->write('Page: '.$page);
+
+    return $response;
+});
+
+$app->run();
