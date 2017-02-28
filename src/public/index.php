d1dd3cc7dee31ba41fc7f92826528f63106b9407
Nick Sergeychuk <nicknixer@gmail.com>
2017-02-28 21:09:14 +0300

    add database in DI

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,32 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
 
-$app = new \Slim\App;
+$container = new \Slim\Container;
+$app = new \Slim\App($container);
+
+$container = $app->getContainer();
+
+$container['settings']['db'] = [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'task',
+        'username' => 'root',
+        'password' => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ];
+
+$container['db'] = function ($container) {
+
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
 
 $app->get('/', function (Request $request, Response $response) {
 
