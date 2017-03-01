c13cebc517ebdd7154ac9c3ec56f7e91866ab06f
Nick Sergeychuk <nicknixer@gmail.com>
2017-03-01 14:11:11 +0300

    refac: move settings to file

--- src/public/index.php
+++ src/public/index.php
@@ -5,20 +5,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
 
-$config = [
-    'settings' => [
-        'db' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'task',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-        ],
-    ],
-];
+$config = require '../settings.php';
 
 $app = new \Slim\App($config);
 
---
+++ src/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'task',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ],
+    ],
+];
